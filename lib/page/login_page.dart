18c23942b0365decc13bcd9a9a2ff89334588a5e
defarrel <Defarrel>
2025-04-22 20:18:52 +0700

    menambahkan variable final untuk email dan password

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -8,7 +8,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
   @override
+  void initState() {
+    super.initState();
+  }
+  
   Widget build(BuildContext context) {
     return const Placeholder();
   }
