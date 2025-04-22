46907f8ad4d930a5754939663fd884348e0298f4
defarrel <Defarrel>
2025-04-22 21:23:06 +0700

    Menambahkan button dengan navigator pushreplacement untuk ke homepage

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -46,6 +46,19 @@ class _LoginPageState extends State<LoginPage> {
                   return null;
                 },
               ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Login'),
+              ),
             ],
           ),
         ),
