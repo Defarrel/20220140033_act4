14609bed1dd69d8d4806bcc8941d80a60d633b94
defarrel <Defarrel>
2025-04-23 09:22:45 +0700

    Menambahkan fungsi untuk menampilkan data order

--- lib/page/detailorder_page.dart
+++ lib/page/detailorder_page.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:play_navigation/page/home_page.dart';
 
 class DetailorderPage extends StatelessWidget {
   final String makanan;
   final String minuman;
   final String jumlahMakanan;
-  final String JumlahMinuman;
+  final String jumlahMinuman;
   final int totalHarga;
   const DetailorderPage({
     super.key,
     required this.makanan,
     required this.minuman,
     required this.jumlahMakanan,
-    required this.JumlahMinuman,
+    required this.jumlahMinuman,
     required this.totalHarga,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Detail Order')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QYT Makanan: $jumlahMakanan'),
+            Text('Drink QYT Minuman: $jumlahMinuman'),
+            Text('Total Harga: $totalHarga'),
+
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                  (route) => false,
+                );
+              },
+              child: Text('Finish Order'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
